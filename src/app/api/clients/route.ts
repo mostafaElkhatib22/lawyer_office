be951e65e3d17ac3a7f7cbe9c713c9d48ca9c45e
mostafaElkhatib22 <mostafaelkhatib26@gmail.com>
2025-09-29 13:18:57 +0300

    ؤء

--- src/app/api/clients/route.ts
+++ src/app/api/clients/route.ts
@@ -3,7 +3,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-require-imports */
 import dbConnect from '@/lib/dbConnect';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import Client from '@/models/Client';
